56cbc94c461fa2fee4f7bec6cf944172ccbafd57
apple <lizi@appledemacbook-pro.local>
2017-11-29 19:38:05 +0800

    Initial Commit

---
+++ 开始刷题/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  开始刷题
+//
+//  Created by lizi on 2017/11/29.
+//  Copyright © 2017年 lizi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
